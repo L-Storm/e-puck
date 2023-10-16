16da6c74931ec94ef1d4809e80ded2690fd227e8
lloyd <lloydstorm1@gmail.com>
2023-10-16 10:50:45 +0100

    add movement

--- ourProject/main.c
+++ ourProject/main.c
@@ -7,8 +7,13 @@
 #include "hal.h"
 #include "memory_protection.h"
 #include <main.h>
+
+//LEDS
 #include "leds.h"
 #include "spi_comm.h"
+
+//MOTORS
+#include "motors.h"
 
 int main(void)
 {
@@ -17,16 +22,36 @@ int main(void)
     chSysInit();
     mpu_init();
 
+//    LEDS
     void clear_leds(void);
     void spi_comm_start(void);
+
+//    MOTORS
+    void motors_init(void);
 
 
     /* Infinite loop. */
     while (1) {
-    	unsigned int ledValue = 2;  // or any other non-negative integer value
-    	set_body_led(ledValue);
-        chThdSleepMilliseconds(1000);
 
+    	int i
+		int motorForward = 500;
+    	int motorBack = -500;
+    	int motorStop = 0;
+
+    	for(i=1; i<=4; i++){
+        	unsigned int ledValue = 2;  // or any other non-negative integer value
+        	set_body_led(ledValue);
+            chThdSleepMilliseconds(1000);
+    	}
+
+
+    	void left_motor_set_speed(motorForward);
+    	void right_motor_set_speed(MotorBack);
+
+        chThdSleepMilliseconds(4000);
+
+    	void left_motor_set_speed(motorStop);
+    	void right_motor_set_speed(MotorStop);
     }
 }
 
