488c0c82edbf108bd5f42df3398c069e79fa7c97
Abdul Rasheed <87432571+Abdulrasheedar@users.noreply.github.com>
2023-10-30 10:51:18 +0000

    Update main.c

--- ourProject/main.c
+++ ourProject/main.c
@@ -6,204 +6,171 @@
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
+//#include "sensors/proximity.h"
 #include <main.h>
 
 //LEDS
-#include "leds.h"
+//#include "leds.h"
 #include "spi_comm.h"
 
 //MOTORS
 #include "motors.h"
 
-#include "sensors/proximity.h"
-
-#define PROX_SENSORS 8
-static int distance_sensors[PROX_SENSORS];
-static const char *distance_sensors_names[PROX_SENSORS] = {"ps0", "ps1", "ps2", "ps3", "ps4", "ps5", "ps6", "ps7"};
-
-#define SENSOR_THRESHOLD 140
-
-/* E-puck angular speed in rad/s */
-#define MAX_SPEED 6.28
-
-#define ROBOT_ANGULAR_SPEED_IN_DEGREES 283.588111888
-
-static int TIME_STEP;
-
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
-int left_motor;
-int right_motor;
+#define ROTATE_MODE 1
+#define NAVIGATE_MODE 2
 
-void robot_controller_init(int time_step)
-{
-	TIME_STEP = time_step;
-  /* get a handler to the motors and set target position to infinity (speed control) */
-  left_motor = wb_robot_get_device("left wheel motor");
-  right_motor = wb_robot_get_device("right wheel motor");
-  wb_motor_set_position(left_motor, INFINITY);
-  wb_motor_set_position(right_motor, INFINITY);
-  wb_motor_set_velocity(left_motor, 0.0);
-  wb_motor_set_velocity(right_motor, 0.0);
 
-  /* get a handler to the sensors */
-	for (int i = 0; i < PROX_SENSORS; i++) {
-			distance_sensors[i] = wb_robot_get_device(distance_sensors_names[i]);
-			wb_distance_sensor_enable(distance_sensors[i], TIME_STEP);
-	}
-}
+int mode;
+int mode = NAVIGATE_MODE;
+int outputRightVelocity;
+int outputLeftVelocity;
+int* outputRightVelocityPtr = &outputRightVelocity;
+int* outputLeftVelocityPtr = &outputLeftVelocity;
 
-static float calculate_rotation_time(float degrees)
-{
-	return abs(degrees) / ROBOT_ANGULAR_SPEED_IN_DEGREES;
-}
 
-/* function to stop the motor (set motor velocity to zero) */
-void motor_stop() {
-  wb_motor_set_velocity(left_motor, 0);
-  wb_motor_set_velocity(right_motor, 0);
-}
+void avoid_collisions(unsigned int noiseLevel, unsigned int activationThreshold, int inputLeftVelocity, int inputRightVelocity) {
+	/* avoid collisions using a vector sum calculation from all 8 proximaty sensors. */
 
-/* function to set motor velocity to move forward */
-void motor_move_forward() {
-  wb_motor_set_velocity(left_motor, MAX_SPEED);
-  wb_motor_set_velocity(right_motor, MAX_SPEED);
-}
+	int i;
+	outputLeftVelocity = inputLeftVelocity;
+	outputRightVelocity = inputRightVelocity;
+	unsigned int sensorValues[8];
+	double xWeights[] = {-1, -0.7, 0, 0.9, 0.9, 0, -0.7, -1};
+	double yWeights[] = {0.3, 0.7, 1, 0.5, -0.5, -1, -0.7, -0.3};
+	int xSensorTotal = 0;
+	int ySensorTotal = 0;
 
-/* function to set motor velocity to rotate right in place*/
-void motor_rotate_right() {
-  wb_motor_set_velocity(left_motor, MAX_SPEED);
-  wb_motor_set_velocity(right_motor, -MAX_SPEED);
-}
-
-/* function to set motor velocity to rotate left in place*/
-void motor_rotate_left() {
-  wb_motor_set_velocity(left_motor, -MAX_SPEED);
-  wb_motor_set_velocity(right_motor, MAX_SPEED);
-}
-
-void motor_rotate_left_in_degrees(float degrees) {
-	motor_rotate_left();
-
-	float duration = calculate_rotation_time(degrees);
-	float start_time = wb_robot_get_time();
-	do
-	{
-		wb_robot_step(TIME_STEP);
-	} while (wb_robot_get_time() < start_time + duration);
-
-	motor_stop();
-}
-
-/* function to get sensors condition
- * if sensor detect obstacle, then the condition is true
- * */
-bool * get_sensors_condition()
-{
-	static bool sensors_condition[PROX_SENSORS] = {false};
-
-	for (int i = 0; i <  PROX_SENSORS; i++) {
-		/*
-		 * Obstacle detected condition is true if the sensor values is larger then the threshold value
-		 * */
-		if (wb_distance_sensor_get_value(distance_sensors[i]) > SENSOR_THRESHOLD) {
-			sensors_condition[i] = true;
-		} else {
-			sensors_condition[i] = false;
+	// Remove noise and readings too far away.
+	for(i = 0; i < 8; i++) {
+		sensorValues[i] = get_calibrated_prox(i);
+		if(sensorValues[i] >= noiseLevel && sensorValues[i] >= activationThreshold) {
+			xSensorTotal += xWeights[i] * sensorValues[i];
+			ySensorTotal += yWeights[i] * sensorValues[i];
 		}
 	}
 
-	return sensors_condition;
-}
-
-/* function to print sensors values
- * */
-void print_sensor_values() {
-	printf("%s sensor values: ", wb_robot_get_name());
-
-	for (int i = 0; i <  ; i++) {
-		printf("%d:%.3f ", i, wb_distance_sensor_get_value(distance_sensors[i]));
+	// Modify the velocity components based on sensor values.
+	if(inputLeftVelocity >= 0) {
+		outputLeftVelocity += ((xSensorTotal) - (ySensorTotal * 0.6));
+	} else {
+		outputLeftVelocity -= ((xSensorTotal) + (ySensorTotal * 0.6));
+	}
+	if(inputRightVelocity >= 0) {
+		outputRightVelocity += ((xSensorTotal) + (ySensorTotal * 0.6));
+	} else {
+		outputRightVelocity -= ((xSensorTotal) - (ySensorTotal * 0.6));
 	}
 
-	printf("\n");
+
+
+	// Update speed.
+	left_motor_set_speed(outputLeftVelocity);
+	right_motor_set_speed(outputRightVelocity);
+}
+unsigned int currentSpeeds[10][2]; // 2D array for circular buffer
+unsigned int i2 = 0;
+void check_stationary() {
+	/* Function records into a buffer the last 10 wheel speeds
+	 * for both wheels. Then if below a threshold, will change
+	 * from NAVIGATION_MODE to ROTATE_MODE. */
+
+    unsigned int sum;
+
+
+    // Get currentSpeedValue
+    int currentSpeedRight = abs(*outputRightVelocityPtr);
+    int currentSpeedLeft = abs(*outputLeftVelocityPtr);
+
+
+    // Update circular buffer.
+    currentSpeeds[i2][0] = currentSpeedLeft;
+    currentSpeeds[i2][1] = currentSpeedRight;
+
+    sum = 0;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            sum += currentSpeeds[i][j];
+        }
+    }
+
+    // if stationary, rotate.
+    if (sum < 800) {
+    	mode = ROTATE_MODE;
+
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                currentSpeeds[i][j] = 100;
+            }
+        }
+
+    }
+
+    // Update index i (circular manner)
+    i2 = (i2 + 1) % 10;
+
+    return;
 }
 
-bool *get_sensors_condition()
-{
-	static bool sensors_condition[PROX_SENSORS] = {false};
+void rotate(int speed, int time) {
 
-	for (int i = 0; i < PROX_SENSORS ; i++) {
-		/*
-		 * Obstacle detected condition is true if the sensor values is larger then the threshold value
-		 * */
-		if (wb_distance_sensor_get_value(distance_sensors[i]) > SENSOR_THRESHOLD) {
-			sensors_condition[i] = true;
-		} else {
-			sensors_condition[i] = false;
-		}
-	}
+	left_motor_set_speed(speed);
+	right_motor_set_speed(-speed);
 
-	return sensors_condition;
+	chThdSleepMilliseconds(time);
+
+	left_motor_set_speed(0);
+	right_motor_set_speed(0);
+
+	mode = NAVIGATE_MODE;
+
 }
 
-/* function to print sensors values
- * */
-void print_sensor_values() {
-	printf("%s sensor values: ", wb_robot_get_name());
-
-	for (int i = 0; i <  ; i++) {
-		printf("%d:%.3f ", i, wb_distance_sensor_get_value(distance_sensors[i]));
-	}
-
-	printf("\n");
-}
 
 
 int main(void)
 {
-	messagebus_init(&bus, &bus_lock, &bus_condvar);
-	proximity_start();
-	calibrate_ir();
-
+	//LITERALLY CAN'T REMEMBER WHAT THESE DO
     halInit();
     chSysInit();
     mpu_init();
-
-
-//    MOTORS
+    //LEDS?
+    spi_comm_start();
+    // PROX SENSORS
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    proximity_start();
+    calibrate_ir();
+    // MOTORS
     motors_init();
 
 
 
-int i;
-int motorForward = 500;
-//int motorBack = -500;
-//int motorStop = 0;
+    //constantly move forward.
+	const int motorForward = 900;
+    left_motor_set_speed(motorForward);
+    right_motor_set_speed(motorForward);
 
-    /* Infinite loop. */
+
+    /* Infinite loop. continuously avoid obstacles unless stuck, then turn.*/
     while (1) {
-    	bool *is_sensors_active = get_sensors_condition();
 
-    			if (is_sensors_active[1] && is_sensors_active[6]) {
-    				motor_rotate_left_in_degrees(180);
-    			} else if (is_sensors_active[0] || is_sensors_active[1]) {
-    				motor_rotate_left();
-    			} else if (is_sensors_active[7] || is_sensors_active[6]) {
-    				motor_rotate_right();
-    			} else {
-    				motor_move_forward();
-    			}
-
-//
-//    	left_motor_set_speed(motorForward);
-//    	right_motor_set_speed(motorBack);
-//
-//        chThdSleepMilliseconds(4000);
-//
-//    	left_motor_set_speed(motorStop);
-//    	right_motor_set_speed(motorStop);
+    	switch (mode) {
+    	    case ROTATE_MODE:
+				rotate(700,800);
+    	        break;
+    	    case NAVIGATE_MODE:
+    	    	avoid_collisions(5, 100, motorForward, motorForward);
+				chThdSleepMilliseconds(100);
+				check_stationary();
+    	        break;
+    	    default:
+    	    	chThdSleepMilliseconds(100);
+    	    	break;
+    	        // code to be executed if expression doesn't match any constant
+    	}
     }
 }
 
